48b31f67cc926f0c69f0c313b3ef99ba2075584e
Rory Devitt <rorytdevitt@gmail.com>
2025-12-09 21:13:25 +0000

    Remove unused analysis code and add URLs from old rounds

--- analysis.py
+++ analysis.py
@@ -1,75 +1,77 @@
 from collections import defaultdict
-from collections.abc import AsyncGenerator
 
-from awards import get_ranked_guesses
-from geoguessr_scraper import get_challenge_scores
+from geoguessr_scraper import GeoguessrClient
 from formatters import format_scoreboard
 
 import dotenv
 
-from models import ChallengeResult
+from settings import get_settings
 
 dotenv.load_dotenv()
 
 CHALLENGE_URLS = [
-    "https://www.geoguessr.com/challenge/uRZJEmW32RDdxRme",
-    "https://www.geoguessr.com/challenge/Dw1FhWM5GKPxxPhl",
-    "https://www.geoguessr.com/challenge/hYq5oMDONO3Jl2Sx",
-    "https://www.geoguessr.com/challenge/lKppru0IWyDZwIAW",
-    "https://www.geoguessr.com/challenge/i9tEGmao2qB684un",
-    "https://www.geoguessr.com/challenge/sh8hlW70FBUHblJo",
-    "https://www.geoguessr.com/challenge/HX1zet3ADxT0s1MF",
+    "https://www.geoguessr.com/challenge/3qfccXf0H2LxVfKd",
+    "https://www.geoguessr.com/challenge/5zDtniWpKjF9cL8I",
+    "https://www.geoguessr.com/challenge/862qIpnYWI3V07S1",
+    "https://www.geoguessr.com/challenge/8YpJQusdcr2DkNjV",
+    "https://www.geoguessr.com/challenge/92KcQzZWwNGfb0eB",
+    "https://www.geoguessr.com/challenge/989O8zGW1iWsfrsu",
+    "https://www.geoguessr.com/challenge/9p8ikbiZRCzt2GEt",
+    "https://www.geoguessr.com/challenge/ALvkMtCyioXjXJfj",
+    "https://www.geoguessr.com/challenge/DT3pVBmF4MVZt0QS",
+    "https://www.geoguessr.com/challenge/EO15fho3VkzB41CX",
+    "https://www.geoguessr.com/challenge/GaE9HCgVPNUVAAtD",
+    "https://www.geoguessr.com/challenge/HhBflrIkGTGPr1uo",
+    "https://www.geoguessr.com/challenge/J2tCBccFnlJq1eUD",
+    "https://www.geoguessr.com/challenge/KGI2gHP15ejmDGVg",
+    "https://www.geoguessr.com/challenge/OEViUK0CsXsO7BcB",
+    "https://www.geoguessr.com/challenge/Rszuuy3Erzra0VVs",
+    "https://www.geoguessr.com/challenge/T3VgMnP0RjOX6PmK",
+    "https://www.geoguessr.com/challenge/TBoioOIrdqrFZJIQ",
+    "https://www.geoguessr.com/challenge/Wl7VfeHU0W6R7Eu3",
+    "https://www.geoguessr.com/challenge/X37AfCqv57u8rGdz",
+    "https://www.geoguessr.com/challenge/b1xLYaAyVSEmR8t9",
+    "https://www.geoguessr.com/challenge/bJMNpBrnnH9tTguW",
+    "https://www.geoguessr.com/challenge/cU0tALpAOGT6iLET",
+    "https://www.geoguessr.com/challenge/ctNxp7ksqHuKyBWd",
+    "https://www.geoguessr.com/challenge/gVmY1NVOqnaHnHy4",
+    "https://www.geoguessr.com/challenge/iOgqSZ2AFXV0k1ku",
+    "https://www.geoguessr.com/challenge/jnPeVIR9cMMcaHKL",
+    "https://www.geoguessr.com/challenge/nDlRf0Nfg9f19lyR",
+    "https://www.geoguessr.com/challenge/o6HtQ5XfgdXzY7hO",
+    "https://www.geoguessr.com/challenge/p3gCJv07XI8WhI2I",
+    "https://www.geoguessr.com/challenge/q1jl07AoTdu9XqVr",
+    "https://www.geoguessr.com/challenge/qYfz8PCNsvSEcEpo",
+    "https://www.geoguessr.com/challenge/rRE1qeaeU7butj9q",
+    "https://www.geoguessr.com/challenge/sGBNv6Sy0axvixlc",
+    "https://www.geoguessr.com/challenge/sgTR0MdmQRJ9Ntdx",
+    "https://www.geoguessr.com/challenge/svZKlxHTVebzd1m8",
+    "https://www.geoguessr.com/challenge/uLRo3Nl2ZbMG3HnL",
 ]
 
 
 async def average_scores():
     totals = defaultdict(int)
+    rounds_played = defaultdict(int)
+    settings = get_settings()
+    client = GeoguessrClient(
+        ncfa_cookie=settings.geoguessr_ncfa_cookie
+    )  # Add valid cookie if needed
+
     for url in CHALLENGE_URLS:
-        result = await get_challenge_scores(url)
+        result = await client.get_challenge_scores(url)
         for player_score in result.scores:
             totals[player_score.player.name] += player_score.gross_score
+            rounds_played[player_score.player.name] += 1
 
     averages = {
-        player: round(total_score / len(CHALLENGE_URLS))
+        player: round(total_score / rounds_played[player])
         for player, total_score in totals.items()
     }
     print(format_scoreboard(averages, header="Average Score"))
 
 
-async def get_results_for_challenges(
-    challenge_ids: list[str],
-) -> AsyncGenerator[ChallengeResult]:
-    for challenge_id in challenge_ids:
-        url = f"https://www.geoguessr.com/challenge/{challenge_id}"
-        try:
-            result = await get_challenge_scores(url)
-            yield result
-        except Exception as e:
-            print(f"Error fetching challenge {challenge_id}: {e}")
-            raise
-
-
-async def analyse_round(challenge_id: str):
-    challenge_url = f"https://www.geoguessr.com/challenge/{challenge_id}"
-    result = await get_challenge_scores(challenge_url)
-    ranked_guesses = get_ranked_guesses(result)
-
-    table_data = {
-        f"{rg.player.name}-{rg.location_index}": rg.adjusted_score
-        for rg in ranked_guesses
-    }
-    print(
-        format_scoreboard(
-            table_data, header=f"Adjusted Scores for Challenge {challenge_id}"
-        )
-    )
-
-
 if __name__ == "__main__":
     import asyncio
 
-    ALL_CHALLENGES = [
-        "862qIpnYWI3V07S1",
-    ]
-
-    asyncio.run(analyse_round(ALL_CHALLENGES[0]))
+    asyncio.run(average_scores())
