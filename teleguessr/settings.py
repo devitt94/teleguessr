f188b5682f075264cb386617e20979c86106568c
Rory Devitt <rorytdevitt@gmail.com>
2026-08-26 22:20:13 +0100

    Reduce betting limits

--- teleguessr/settings.py
+++ teleguessr/settings.py
@@ -39,8 +39,8 @@ class ModelSettings(BaseModel):
     n_sims: int = 40_000
     overround: float = 0.25
     max_profit_self: float = 100.0
-    max_profit_non_self: float = 30.0
-    max_loss_self: float = 0.0
+    max_profit_non_self: float = 25.0
+    max_loss_self: float = 25.0
     max_loss_non_self: float = 100.0
     min_odds_for_other_bet: float = 4.0
     min_profit_bet: float = 1.0
